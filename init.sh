c24494d15329fafe535d54aa5587e93dc0073eed
Luiz F. Lima <lf.system@outlook.com>
2022-09-12 20:33:04 -0300

    Configuração de ambiente com docker

---
+++ init.sh
@@ -0,0 +1,8 @@
+# copia .env.example para .env
+cp .env.example .env
+
+
+# executa o comando php artisan key:generate
+# executa o comando php artisan migrate
+# executa o comando php artisan db:seed
+
